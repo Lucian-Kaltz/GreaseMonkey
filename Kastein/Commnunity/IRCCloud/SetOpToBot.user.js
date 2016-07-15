3067f16547862524a9c1c468f26cc021d97a9f19
Lucian-Kaltz <povslusian@gmail.com>
2016-07-15 18:53:01 +0900

    Create SetOpToBot.user.js

---
+++ Kastein/Commnunity/IRCCloud/SetOpToBot.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         IRCCloud 봇 오퍼레이터 권한 설정
+// @namespace   org.metalscraps.kastein.community.irccloud.setoptobot
+// @version      1.0
+// @description  웹페이지 접근시 DOM 읽어서 봇한테 옵 없으면 넣어줌. 자체 API도 나중에 확인해봐야함.
+// @author       povslusian@gmail.com
+// @match        https://www.irccloud.com/*
+// @updateURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
+// @downloadURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
+// @run-at document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // 이벤트 방식으로 변경해야함
+    setTimeout(function(){
+        if(!$("button[data-name=Kastein_Bot]").data("mode").includes("o")) $($("textarea[id^=bufferInputView]:visible").val("/op Kastein_Bot")[0].form).submit();
+    }, 10000);
+})();
