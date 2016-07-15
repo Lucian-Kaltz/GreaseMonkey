0c835a5083325dd6a1c8592d81762daece9f4adb
Lucian-Kaltz <povslusian@gmail.com>
2016-07-15 19:02:17 +0900

    Update SetOpToBot.user.js

--- Kastein/Community/IRCCloud/SetOpToBot.user.js
+++ Kastein/Community/IRCCloud/SetOpToBot.user.js
@@ -5,8 +5,8 @@
 // @description  웹페이지 접근시 DOM 읽어서 봇한테 옵 없으면 넣어줌. 자체 API도 나중에 확인해봐야함.
 // @author       povslusian@gmail.com
 // @match        https://www.irccloud.com/*
-// @updateURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
-// @downloadURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
+// @updateURL https://raw.githubusercontent.com/Lucian-Kaltz/GreaseMonkey/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
+// @downloadURL https://raw.githubusercontent.com/Lucian-Kaltz/GreaseMonkey/master/Kastein/Community/IRCCloud/SetOpToBot.user.js
 // @run-at document-start
 // @grant        none
 // ==/UserScript==
