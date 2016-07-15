24a65f61fdbf45ecee5490b24b8f16eed6e4df2f
Lucian-Kaltz <povslusian@gmail.com>
2016-07-15 19:01:48 +0900

    Update CaptureLink.user.js

--- Kastein/Community/IRCCloud/CaptureLink.user.js
+++ Kastein/Community/IRCCloud/CaptureLink.user.js
@@ -5,17 +5,20 @@
 // @version     1.0
 // @match     *://www.irccloud.com*
 // @author       povslusian@gmail.com
-// @updateURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/CaptureLink.user.js
-// @downloadURL https://github.com/Lucian-Kaltz/GreaseMonkey/raw/master/Kastein/Community/IRCCloud/CaptureLink.user.js
+// @updateURL https://raw.githubusercontent.com/Lucian-Kaltz/GreaseMonkey/master/Kastein/Community/IRCCloud/CaptureLink.user.js
+// @downloadURL https://raw.githubusercontent.com/Lucian-Kaltz/GreaseMonkey/master/Kastein/Community/IRCCloud/CaptureLink.user.js
 // @run-at document-start
 // @grant       none
 // @require     https://cdn.socket.io/socket.io-1.2.0.js
 // ==/UserScript==
-if (document.domain == "www.irccloud.com") {
-    socket = io.connect("http://localhost:9092/screenshot");
-    socket.on("message", function(data){
-        x = $("div.buffercontainer.active").not(".buffercontainer--hidden").find("form.input");
-        x.find("textarea").val(data+".png");
-        x.submit();
-    });
-}
+
+(function () {
+	if (document.domain == "www.irccloud.com") {
+		socket = io.connect("http://localhost:9092/screenshot");
+		socket.on("message", function (data) {
+			x = $("div.buffercontainer.active").not(".buffercontainer--hidden").find("form.input");
+			x.find("textarea").val(data + ".png");
+			x.submit();
+		});
+	}
+})();
